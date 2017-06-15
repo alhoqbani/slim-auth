3c36d03b087d1f9e8362275e5d107e89d5936a47
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-06-14 20:28:49 -0700

    Basic Struture

---
+++ bootstrap/app.php
@@ -0,0 +1,12 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+
+$app = new Slim\App([
+    'settings' => [
+        'displayErrorDetails' => true,
+    ],
+]);
+
+
+require_once __DIR__ . '/../routes/web.php';
